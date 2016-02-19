47d891556f2314166c4800fa6f9bca1739c38c32
Adrian55 <adri96lp@hotmail.com>
2016-02-19 04:37:30 +0100

    Respuesta con Cuestionario y Preguntas

--- api/controllers/RespuestaController.js
+++ api/controllers/RespuestaController.js
@@ -6,28 +6,19 @@
  */
 
 module.exports = {
-	
+
+	load: function(req, res, next) {
+		Respuesta.findOne({
+			where: { id: Number(req.params.respuestaId)}
+		}).then(function(respuesta){
+			if(respuesta) {
+				req.respuesta = respuesta;
+				next();
+			} else { next(new Error('No existe la respuesta con el id' + req.params.respuestaId));}
+		}).catch(function(error){next(error);});
+	}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
 };
 
