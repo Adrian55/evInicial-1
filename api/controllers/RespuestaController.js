a828a7f6f81cfb7df0b5247e5e8f7e75abeb480f
Adrian55 <adri96lp@hotmail.com>
2016-02-23 05:59:58 +0100

    Cambios Avanzados/Probando Funcionamiento

--- api/controllers/RespuestaController.js
+++ api/controllers/RespuestaController.js
@@ -7,18 +7,48 @@
 
 module.exports = {
 
-	load: function(req, res, next) {
-		Respuesta.findOne({
-			where: { id: Number(req.params.respuestaId)}
-		}).then(function(respuesta){
-			if(respuesta) {
-				req.respuesta = respuesta;
-				next();
-			} else { next(new Error('No existe la respuesta con el id' + req.params.respuestaId));}
-		}).catch(function(error){next(error);});
-	}
+	almacenaRespuesta: function(req, res, next) {
+		var respuestaRecibida = req.body.answered;
+		var valor1;
+		var valor2;
+		
+			
+		Opcion.findOne({
+			where: { id: Number(respuestaRecibida) }
+		}).populate('subopciones').then(function(opcion){
+				
 
+			opcion.subopciones.forEach(function(subopcion){
+				
+				if(subopcion.nombre=='responserequired'){
+					valor1=subopcion.valor; 
+					
+				}
+				if(subopcion.nombre=='responsefieldlines'){
+					valor2=subopcion.valor; 
+					
+				}
+				});
+				console.log(req.session.passport.user);
 
+				Alumno.findOne({
+					where: {user: req.session.passport.user}
+				}).then(function(alumno){
+					console.log(req.session.passport.user);
+					if(alumno){
+
+			Respuesta.create({valor: valor2, puntuacion:valor1, 
+				cuestionario: req.params.cuestionarioId, pregunta: req.params.preguntaId,
+				 alumno: alumno.id }).exec(function createCB(err, created){
+  				res.send('Valor' + created.valor + ' Puntuacion' + created.puntuacion + ' Cuestionario' + created.cuestionario + ' pregunta' + created.pregunta + ' alumno' + created.alumno);
+
+				});
+		}else{
+			res.send("No estas auntenticado como alumno");
+		}
+		});
+		
+	})
+}
 
 };
-
--- api/models/Pregunta.js
+++ api/models/Pregunta.js
@@ -28,6 +28,57 @@ module.exports = {
         collection:'opcion',
         via:'pregunta'
     }
-  }
+  },
+  aJSON: function(cb) {
+        switch(this.tipo) {
+          case 'essay':
+              return this.essayToJSON();
+              break;
+          case 'matching':
+              this.matchingToJSON(function(PreguntaJSON){cb(PreguntaJSON)});
+              break;
+          case 'multichoice':
+              this.multichoiceToJSON(function(PreguntaJSON){cb(PreguntaJSON)});
+              break;
+          case 'numerical':
+              this.numericalToJSON(function(PreguntaJSON){cb(PreguntaJSON)});
+              break;
+          case 'shortanswer':
+              this.shortanswerToJSON(function(PreguntaJSON){cb(PreguntaJSON)});
+              break;
+          case 'truefalse':
+              this.truefalseToJSON(function(PreguntaJSON){cb(PreguntaJSON)});
+              break;
+          default:
+              break;
+        }      
+    },
+    
+    essayToJSON: function(cb) {
+        var PreguntaJSON = this.toJSON();
+        return Opcion.find().where({ pregunta: this.id })
+        .populate('subopciones');
+    },
+
+    matchingToJSON: function(cb) {
+        
+    },
+
+    multichoiceToJSON: function(cb) {
+
+    },
+
+    numericalToJSON: function(cb) {
+
+    },
+
+    shortanswerToJSON: function(cb) {
+
+    },
+
+    truefalseToJSON: function(cb) {
+
+    }
+
 };
 
